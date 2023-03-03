c738976a305b9b7a536575f70823a163904822d9
cone011 <32248132+cone011@users.noreply.github.com>
2023-03-03 14:36:43 -0300

    Update removeElements.js

--- removeElements.js
+++ removeElements.js
@@ -1,36 +1,32 @@
 var removeElements = function (head, val) {
   if (!head) return null;
-  function getNodes(node) {
-    if (node.next === null) return;
-    let currentNode = node.next;
-    let previousNode = node;
-    if (currentNode.val === val) {
-      currentNode = currentNode.next;
-      previousNode.next = currentNode;
-    } else {
-      previousNode = currentNode;
-      currentNode = currentNode.next;
-    }
-    return getNodes(node.next);
-  }
-  getNodes(head);
+  let current = head.next;
+  let previous = head;
   if (head.val === val) return head.next;
+  while (current) {
+    if (current.val === val) {
+      current = current.next;
+      previous.next = current;
+    } else {
+      previous = current;
+      current = current.next;
+    }
+  }
   return head;
 };
 
-// var removeElements = function(head, val) {
-//     if(!head)return null;
-//     let cur = head.next,
-//         prev = head;
-//     while(cur){
-//         if(cur.val === val){
-//             cur = cur.next;
-//             prev.next = cur;
-//         }else{
-//             prev = cur;
-//             cur = cur.next
-//         }
-//     }
-//     if(head.val === val)return head.next;
-//     return head;
-// };
+//Recursive way
+var removeElements = function (head, val) {
+  //First we check if the head have some value if not we return null
+  if (!head) return null;
+  //Second we check if the value is equal to the value are passing our function for remove
+  if (head.val === val) {
+    //if are equal we change the current position which have the value to remove
+    //and we swap for the next node
+    head = removeElements(head.next, val);
+  } else {
+    //if not equal we dont make any change we only go for the next node
+    head.next = removeElements(head.next, val);
+  }
+  return head;
+};
